f76fa9da6706f101133b7ce190757338e92d8155
yuriyyatseyko <yuriy.yatseyko@incorainc.com>
2019-12-20 16:23:29 +0200

    Initial commit

---
+++ Example/FishNetFramework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FishNetFramework
+//
+//  Created by yuriyyatseyko on 12/20/2019.
+//  Copyright (c) 2019 yuriyyatseyko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
